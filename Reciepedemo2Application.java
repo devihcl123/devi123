a80b6ef12c3368b6f5c7b6f2b023d3b479a91cef
devihcl123 <48594732+devihcl123@users.noreply.github.com>
2019-03-17 22:49:19 +0530

    Add files via upload

---
+++ Reciepedemo2Application.java
@@ -0,0 +1,13 @@
+package com.reciepe.demo2.Reciepedemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reciepedemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reciepedemo2Application.class, args);
+	}
+
+}
